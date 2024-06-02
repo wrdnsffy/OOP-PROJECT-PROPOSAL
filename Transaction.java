7005d99fba4f403f0087d00a7ede1cba851cf79b
wrdnsffy <wardina2206@gmail.com>
2024-06-02 13:46:41 +0800

    Transaction.java

--- Transaction.java
+++ Transaction.java
@@ -1,3 +1,5 @@
+//by Wan Nur Irdina Binti Wan Hasbullah 24000247
+
 class Transaction {
     private double amount;
     private double balance;
@@ -8,27 +10,21 @@ class Transaction {
       
     }
 
-   
-    public double getAmount() { 
-        
+    public double getAmount() {    
         
         return amount;
-        
-        
+             
     }
-    public void setAmount(double amount) {
-        
+    public void setAmount(double amount) {  
         
         this.amount = amount;
-        
-        
+             
     }
 
     public double getBalance() {
         
         return balance; 
-        
-        
+                
     }
     public void setBalance(double balance) {
         
@@ -37,13 +33,11 @@ class Transaction {
     public String getDate() {
         
         return date; 
-        
-        
+            
     }
     public void setDate(String date) { 
         
         this.date = date; 
-        
-        
+           
     }
 }
