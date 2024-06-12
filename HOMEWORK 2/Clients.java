37731593bc600fec8dcb534e2266101bcd3a0be0
wrdnsffy <wardina2206@gmail.com>
2024-06-12 23:08:57 +0800

    Clients.java

--- HOMEWORK 2/Clients.java
+++ HOMEWORK 2/Clients.java
@@ -1,5 +1,8 @@
 //by Wan Nur Irdina Binti Wan Hasbullah (24000247) 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Clients {
     private List<Client> clientList;
 
@@ -16,7 +19,6 @@ public class Clients {
 
     // Method to remove a client from the list
     public void remove(String userId) {
-        
         // Find client by userId and remove from the list
         Client clientToRemove = find(userId);
         if (clientToRemove != null) {
@@ -45,30 +47,32 @@ public class Clients {
         return null; // Return null if client not found
     }
 
-    //test code by Wardina Saffiya Binti Jamalulil (24000996) 
+//test code by Wardina Saffiya Binti Jamalulil (24000996) 
     
-    public static void main(String[] args) {
-        Clients clients = new Clients();
+public static void main(String[] args) {
+    Clients clients = new Clients();
 
-        // Adding clients
-        clients.add(new Client("11", "Raheem", "Raheem@gmail.com"));
-        clients.add(new Client("21", "Dahlia", "dahlia@gmail.com"));
+    // Adding clients
+    clients.add(new Client(new Client.Name("11", "Raheem"), "Raheem@gmail.com", "123456", "1234567890", new Client.Address("123 Jalan Muda", "Ipoh", "Perak", "12345")));
+    clients.add(new Client(new Client.Name("21", "Dahlia"), "Dahlia@gmail.com", "123456", "1234567890", new Client.Address("123 Jalan Muda", "Ipoh", "Perak", "12345")));
 
-        // Printing all clients
-        System.out.println("All Clients: ");
-        clients.print();
+    // Printing all clients
+    System.out.println("All Clients: ");
+    clients.print();
 
-        // Finding a client
-        System.out.println("Find Client with userId 1:");
-        Client foundClient = clients.find("1");
-        if (foundClient != null) {
-            System.out.println(foundClient);
-        } else {
-            System.out.println("Client not found.");
+    // Finding a client
+    System.out.println("\nFind Client with userId 11:");
+    Client foundClient = clients.find("11");
+    if (foundClient != null) {
+        System.out.println(foundClient);
+    } 
+    else {
+        System.out.println("Client not found.");
         }
-      
-        clients.remove("1");
-        System.out.println("All Clients after removing client with userId 1:");
-        clients.print();
+
+    // Removing a client
+    clients.remove("11");
+    System.out.println("\nAll Clients after removing client with userId 11:");
+    clients.print();
     }
 }
